7fa179d7084e54725ba8b0ed3cd1016b52b41bb9
Lorenzo Aceti <lorenzo.aceti@isdbflowtech.com>
2023-07-05 16:28:59 -0300

    Add index.js file

---
+++ index.js
@@ -0,0 +1,40 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended', 'standard', 'plugin:prettier/recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+          },      
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 80,
+          tabWidth: 2,
+          trailingComma: 'all',
+          singleQuote: true,
+          arrowParens: 'always',
+          semi: false,
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  };
+  
