a7c9b96197902e9af98316b36c45d03ae7a61973
Lorenzo <lorenzo.acetii@gmail.com>
2023-07-19 21:38:18 -0300

    Update package and README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# eslint-config-lorenzoa7
+# Lorenzoa7 ESLint Config
+
+## What is included in this package?
+
+- Standard config
+- React plugin
+- React Hooks plugin
+- Prettier for ESLint
+
+## Setup
+
+Install dependencies:
+```
+npm i -D eslint @lorenzoa7/eslint-config
+```
+Inside `.eslintrc.json`:
+```
+{
+  "extends": [
+    "@lorenzoa7/eslint-config", 
+    "next/core-web-vitals"
+  ]
+}
+```
--- index.js
+++ index.js
@@ -1,52 +1,45 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    settings: {
-      react: {
-        version: 'detect',
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    },
+  },
+  extends: [
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        trailingComma: 'all',
+        singleQuote: true,
+        arrowParens: 'always',
+        semi: false,
+        endOfLine: 'auto',
       },
-      'import/parsers': {
-        [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
-      },
-    },
-    extends: [
-      'plugin:react/recommended', 
-      'plugin:react-hooks/recommended', 
-      'standard', 
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended'
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-          },      
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    plugins: [
-      'react', 
-      '@typescript-eslint'
-    ],
-    rules: {
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      'prettier/prettier': [
-        'error',
-        {
-          printWidth: 80,
-          tabWidth: 2,
-          trailingComma: 'all',
-          singleQuote: true,
-          arrowParens: 'always',
-          semi: false,
-          endOfLine: 'auto',
-        },
-      ],
-    },
-  };
-  
+  },
+};
